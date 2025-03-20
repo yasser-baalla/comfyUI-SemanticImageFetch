5e1f9de2f1da2b9988d59b2013fcc4408c92eed4
Yasser Baalla <70327506+yasser-baalla@users.noreply.github.com>
2025-03-20 11:17:07 +0100

    the color grading node

--- __init__.py
+++ __init__.py
@@ -1,43 +1,6 @@
-import torch
+from .nodes import ColorGradingLatent, SemanticImageFetch
 
-class SemanticImageFetch:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "image": ("IMAGE", {"tooltip": "The list of images to fetch the semantic map from."}), 
-                "clip": ("CLIP", {"tooltip": "The CLIP model used for encoding the text."}),
-                "prompt": ("STRING",{"multiline": True} ),
-                "clip_vision": ("CLIP_VISION", {"tooltip": "The CLIPVision model used for encoding the images."}),
-                "number_of_candidates": ("INT", {"default": 1, "min": 1, "max": 10, "tooltip": "Number of closest images to retrieve."}),
-            }
-        }
-    RETURN_TYPES = ("IMAGE",)
 
-    FUNCTION = "fetch"
 
-    CATEGORY = "IMAGE"
-    DESCRIPTION = "Select the closest images to an input prompt"
-    
-    def fetch(self, image, clip, clip_vision, prompt, number_of_candidates):
-        # Encode the prompt using the CLIP model
-        tokens = clip.tokenize(prompt)
-        clip_embed = clip.encode_from_tokens_scheduled(tokens)[0][1]['pooled_output']
-        
-        # Encode the images using the CLIP model
-        # check if the projected image is the one we want
-        image_embed = clip_vision.encode_image(image)['image_embeds']
-        
-        clip_embed = clip_embed / clip_embed.norm(dim=-1, keepdim=True)
-        image_embed = image_embed / image_embed.norm(dim=-1, keepdim=True)
-
-        # Compute cosine similarity
-        similarities = torch.matmul(clip_embed, image_embed.T).reshape(-1)
-
-        # Select the top-k closest images
-        top_k_indices = torch.topk(similarities, k=number_of_candidates, dim=0).indices.squeeze(0)
-        return (image[top_k_indices], )
-    
-
-NODE_CLASS_MAPPINGS = {"SemanticImageFetch": SemanticImageFetch}
-NODE_DISPLAY_NAME_MAPPING = {"SemanticImageFetch": "Semantic Image Fetch"}
+NODE_CLASS_MAPPINGS = {"ColorGrading": ColorGradingLatent, "SemanticImageFetch": SemanticImageFetch}
+NODE_DISPLAY_NAME_MAPPING = {"ColorGrading": "Color Grading", "SemanticImageFetch": "Semantic Image Fetch" }
